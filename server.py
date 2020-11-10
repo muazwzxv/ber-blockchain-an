be4903b93229f262477eaa5b80faaab618487110
muaz wazir <muazwazir@muazs-MacBook-Pro.local>
2020-11-10 15:16:38 +0800

    flask setup and POST transac endpoint

--- server.py
+++ server.py
@@ -1,7 +1,26 @@
+from lib.blockchain import *
+from flask import Flask, request
+import requests
+import time
 
-import sys
-sys.path.append(".")
+# Setup flask
+app = Flask(__name__)
 
-from lib.block import Block
+# Setup blockchain Object
+chain = Blockchain()
+print(chain)
 
 
+@app.route("/tranaction", methods=["POST"])
+def new_transaction():
+    data = request.get_json()
+    fields = ["author", "content"]
+
+    for field in fields:
+        if not data.get(field):
+            return "Invalid Transaction data", 404
+    
+    data["time"] = time.time()
+    chain.add(data)
+
+    return "Success", 201
