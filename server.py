b6267571ad04c53202b0cce9bb4022380f45ac2a
muaz wazir <muazwazir@muazs-MacBook-Pro.local>
2020-11-10 15:23:55 +0800

    get Chain endpoint

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from lib.blockchain import *
 from flask import Flask, request
 import requests
 import time
+import json
 
 # Setup flask
 app = Flask(__name__)
@@ -19,8 +20,18 @@ def new_transaction():
     for field in fields:
         if not data.get(field):
             return "Invalid Transaction data", 404
-    
+
     data["time"] = time.time()
     chain.add(data)
 
     return "Success", 201
+
+
+@app.route("/chain", methods=["GET"])
+def get_chain():
+    chain_data = []
+
+    for block in chain:
+        chain_data.append(block.__dict__)
+
+    return json.dumps({"length": len(chain_data), "chain": chain_data})
